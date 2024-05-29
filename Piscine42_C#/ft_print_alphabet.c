97c40b01e6917db9abf2fc20a29ce2bc39b9a8a5
alexandre chavrier <alex69.chavrier@gmail.com>
2024-05-29 12:06:44 +0200

    ajout d'exercices faits à la piscine de l'école 42 Auvergne-Rhône-Alpes

---
+++ Piscine42_C#/ft_print_alphabet.c
@@ -0,0 +1,19 @@
+#include <unistd.h>
+
+void    ft_print_alphabet(void)
+{
+    char c = 'A';
+
+    while(c <= 'Z')
+    {
+        write(1, &c, 1);
+        c++;
+    }
+    write(1, "\n", 1);
+}
+
+int     main(void)
+{
+    ft_print_alphabet();
+    return(0);
+}
